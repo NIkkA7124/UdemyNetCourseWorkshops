94fa1be28604eb0e9946146f536f004b4e06509e
Dominika Pawlik <nikaa7124@gmail.com>
2021-11-25 23:36:55 +0100

    changed program.cs

--- Module02BasicsCsharp/Ex01Types/Program.cs
+++ Module02BasicsCsharp/Ex01Types/Program.cs
@@ -6,7 +6,7 @@ namespace Ex01Types
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("ex01");
         }
     }
 }
