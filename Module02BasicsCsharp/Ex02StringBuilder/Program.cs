2df25696356377ba2d6114eb14c42bf056700aaf
Dominika Pawlik <nikaa7124@gmail.com>
2021-11-25 23:28:08 +0100

    created empty projects

---
+++ Module02BasicsCsharp/Ex02StringBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex02StringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
