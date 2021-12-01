6587c982b38a6b7783a6ba36fd48026aa30399c4
Dominika Pawlik <nikaa7124@gmail.com>
2021-12-01 22:14:44 +0100

    New code lines added

--- Module02BasicsCsharp/Ex01Types/Program.cs
+++ Module02BasicsCsharp/Ex01Types/Program.cs
@@ -7,6 +7,16 @@ namespace Ex01Types
         static void Main(string[] args)
         {
             Console.WriteLine("ex01");
+
+            Console.WriteLine("What's your name?");
+            string userneme = Console.ReadLine();
+            Console.WriteLine("Hello!");
+            Console.WriteLine(userneme);
+
+            string mood = "Nice to meet you :)";
+
+            Console.WriteLine(mood);
+
         }
     }
 }
