f9deb4666406ace40da9382a390eb2d5852d031c
Dominika Pawlik <nikaa7124@gmail.com>
2021-12-01 23:05:44 +0100

    New code added

--- Module02BasicsCsharp/Ex02StringBuilder/Program.cs
+++ Module02BasicsCsharp/Ex02StringBuilder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Ex02StringBuilder
 {
@@ -6,7 +7,37 @@ namespace Ex02StringBuilder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ex2");
+
+            string message1;
+            message1 = "add new text";  //now the operation above will work
+            Console.WriteLine(message1);
+
+            string message2 = null;
+            Console.WriteLine(message2);
+
+            string message3 = string.Empty;
+            string text = "He said to me \"The weather is nice today ";
+            Console.WriteLine(text);
+
+            string windowslocation = "c:\\windows";
+            string fontsfolder = @"c:\windows\fonts";
+
+            string concatenated = string.Concat(text, "isn't it?\"");
+            Console.WriteLine(concatenated);
+            string concatenated2 = text + "isn't it?\"";
+            Console.WriteLine(concatenated2);
+            string interpolated = $"{text} isn't it?\"";
+
+            StringBuilder sb = new StringBuilder("This ");
+            sb.Append("is ");
+            sb.Append("a ");
+            sb.Append("very ");
+            sb.Append("long ");
+            sb.Append("text");  
+
+            string result = sb.ToString();
+            Console.WriteLine(result);
         }
     }
 }
---
+++ Module02BasicsCsharp/Ex02StringBuilder/StringBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex02StringBuilder
+{
+    internal class String
+    {
+        internal void Append(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
