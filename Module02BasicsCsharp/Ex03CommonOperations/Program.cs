e64c63776312b04ec12b1fe887fe64deffbe3bc9
Dominika Pawlik <nikaa7124@gmail.com>
2021-12-01 23:16:40 +0100

    created empty project

---
+++ Module02BasicsCsharp/Ex03CommonOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex03CommonOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
