9f382667186ae36e2cca279000273b97aa1f3ca7
Dominika Pawlik <nikaa7124@gmail.com>
2021-12-08 21:07:50 +0100

    new code

---
+++ Module02BasicsCsharp/Ex04IfElseStatement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex04IfElseStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Year of birth?");
+            string userinput = Console.ReadLine();
+
+            int yearofbirth = int.Parse(userinput);
+
+            bool isuserover18 = DateTime.Now.Date.Year - yearofbirth >18;
+            if (isuserover18)
+
+            {
+                Console.WriteLine("Hello!");
+            }
+            else if (DateTime.Now.DayOfWeek == DayOfWeek.Wednesday) 
+
+            { 
+                Console.WriteLine("Today is Wednesday"); 
+            }
+
+            else
+
+            {
+                Console.WriteLine("Acces denied");
+            }
+            Console.WriteLine("Bye");
+        }
+        
+    }
+}
---
+++ Module02BasicsCsharp/Ex05SwitchStatement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex05SwitchStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (DateTime.Now.DayOfWeek)
+            {
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Today is Monady :(");
+                    break;
+
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("Today is Tuesday");
+                    break;       
+
+                case DayOfWeek.Friday:
+                    Console.WriteLine("The last day od the work week! :)");
+                    break;
+
+                case DayOfWeek.Saturday:
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("It's weekend! :)");
+                    break;
+
+                default:
+                    Console.WriteLine("It's a middle of the work week");
+                    break;
+
+             
+            }
+
+            }
+        }
+}
