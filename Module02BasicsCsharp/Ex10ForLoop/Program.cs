a34eec04d54e6aea32b1d99d88e1c5ec287d9f8c
Dominika Pawlik <nikaa7124@gmail.com>
2021-12-09 17:14:09 +0100

    new code

---
+++ Module02BasicsCsharp/Ex10ForLoop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex10ForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "Mazda", "Audi", "BMW" };
+
+            int i = 0;
+            while (i < cars.Length)
+            {
+                Console.WriteLine(cars[i]);
+                if (cars[i] == "Audi")
+                {
+                    Console.WriteLine("Bye");
+                    break;
+                }
+                i++;
+            }
+
+            Console.WriteLine("--------------------------------------------");
+
+            for (int j = 0; j < cars.Length; j++)
+            {
+                Console.WriteLine(cars[j]);
+                if (cars[j] == "Audi")
+                {
+                    Console.WriteLine("Bye");
+                    break;
+                }
+
+            }
+        }
+    }
+}
---
+++ Module02BasicsCsharp/Ex11ForeachLoop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex11ForeachLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
